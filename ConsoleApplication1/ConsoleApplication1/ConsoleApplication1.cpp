57ae8762c3b8b6f16273ffdc1159603005febb2c
andop4 <andop4@gmail.com>
2017-05-12 10:58:58 +0300

    yeni repo

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "yeni repo" << endl;
+	return 0;
+}
+
