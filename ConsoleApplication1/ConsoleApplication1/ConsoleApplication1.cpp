18efb2fe6ce10927ad0f1005696679d7d9ab13b7
Sercan <sercancayir42@gmail.com>
2017-05-12 11:22:17 +0300

    devam ettim

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "yeni repo" << endl;
-	cout << "degisiklik" << endl;
+	cout << "degisiklik" << " devam ettim" << endl;
 	return 0;
 }
 
